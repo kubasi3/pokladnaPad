817acb450dd190b3c03f4161128c686d6f234914
kubasi3 <jakub@semeniuk.cz>
2019-10-17 21:52:26 +0200

    ukradeno a upravení candys

--- src/com/company/pad/candys.java
+++ src/com/company/pad/candys.java
@@ -1,4 +1,26 @@
 package com.company.pad;
 
-public class candys {
+public class candys extends item {
+    private String type;
+
+    public candys(String name, float price, int count, String type) {
+        super(name, price, count);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "item{" +
+                "name='" + this.getName() + '\'' +
+                ", id='" + this.getId() + '\'' +
+                "type='" + this.type + '\'' +
+                ", price=" + this.getPrice() +
+                ", count=" + this.getCount() +
+                '}';
+    }
+}
 }
